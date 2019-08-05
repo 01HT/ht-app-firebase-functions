62f8d4aa16717980cc4917bf0fb4c28a79023e6a
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2019-08-05 12:26:59 +0300

    make ts version for firebase functions

--- middleware.js
+++ middleware.js
@@ -1,75 +1,78 @@
-import { express } from "express";
-import { fetch } from "node-fetch";
-import { url } from "url";
-import { browserNotSupported } from "@01ht/ht-app-browser-not-supported";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const fetch = require("node-fetch");
+const url = require("url");
+const ht_app_browser_not_supported_1 = require("@01ht/ht-app-browser-not-supported");
 function middleware(pwashell, appName, domain, svg, ico32, ico64) {
-  const generateUrl = request => {
-    return url.format({
-      protocol: "https",
-      host: domain,
-      pathname: request.originalUrl
-    });
-  };
-  const checkForBots = userAgent => {
-    const botList = "googlebot|yandex|bingbot|duckduckbot|slurp|baiduspider|facebookexternalhit|twitterbot|rogerbot|linkedinbot|embedly|quora link preview|showyoubot|outbrain|pinterest|vkShare|TelegramBot|WhatsApp|W3C_Validator|slackbot|facebot|developers.google.com/+/web/snippet/".toLowerCase();
-    if (userAgent.toLowerCase().search(botList) !== -1) return true;
-    return false;
-  };
-
-  const isIE = userAgent => {
-    const msie = userAgent.indexOf("MSIE "); // IE 10 or older
-    const trident = userAgent.indexOf("Trident/"); // IE 11
-    return msie > 0 || trident > 0;
-  };
-
-  const app = express();
-
-  const renderUrl = "https://render-tron.appspot.com/render";
-
-  app.get("*", (req, res) => {
-    try {
-      const userAgent = req.headers["user-agent"];
-      const botResult = checkForBots(userAgent);
-      const path = url.parse(req.url).pathname;
-      if (path === "/404") res.status(404);
-      if (botResult) {
-        const targetUrl = generateUrl(req);
-        targetUrl.replace("robots.txt", "");
-        // console.log(`${renderUrl}/${targetUrl}?wc-inject-shadydom=true`);
-        fetch(`${renderUrl}/${targetUrl}?wc-inject-shadydom=true`)
-          .then(function(fetchResponse) {
-            if (fetchResponse.status === 404) res.status(404);
-            return fetchResponse.text();
-          })
-          .then(function(body) {
-            res.set("Cache-Control", "public, max-age=300, s-maxage=600");
-            res.set("Vary", "User-Agent");
-            res.send(body.toString());
-          })
-          .catch(err => {
-            throw new Error(err);
-          });
-      } else {
-        if (isIE(userAgent)) {
-          res.send(browserNotSupported(appName, ico64, ico32, svg));
-        } else {
-          res.send(pwashell);
+    const generateUrl = request => {
+        return url.format({
+            protocol: "https",
+            host: domain,
+            pathname: request.originalUrl
+        });
+    };
+    const checkForBots = userAgent => {
+        const botList = "googlebot|yandex|bingbot|duckduckbot|slurp|baiduspider|facebookexternalhit|twitterbot|rogerbot|linkedinbot|embedly|quora link preview|showyoubot|outbrain|pinterest|vkShare|TelegramBot|WhatsApp|W3C_Validator|slackbot|facebot|developers.google.com/+/web/snippet/".toLowerCase();
+        if (userAgent.toLowerCase().search(botList) !== -1)
+            return true;
+        return false;
+    };
+    const isIE = userAgent => {
+        const msie = userAgent.indexOf("MSIE "); // IE 10 or older
+        const trident = userAgent.indexOf("Trident/"); // IE 11
+        return msie > 0 || trident > 0;
+    };
+    const app = express();
+    const renderUrl = "https://render-tron.appspot.com/render";
+    app.get("*", (req, res) => {
+        try {
+            const userAgent = req.headers["user-agent"];
+            const botResult = checkForBots(userAgent);
+            const path = url.parse(req.url).pathname;
+            if (path === "/404")
+                res.status(404);
+            if (botResult) {
+                const targetUrl = generateUrl(req);
+                targetUrl.replace("robots.txt", "");
+                // console.log(`${renderUrl}/${targetUrl}?wc-inject-shadydom=true`);
+                fetch(`${renderUrl}/${targetUrl}?wc-inject-shadydom=true`)
+                    .then(function (fetchResponse) {
+                    if (fetchResponse.status === 404)
+                        res.status(404);
+                    return fetchResponse.text();
+                })
+                    .then(function (body) {
+                    res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+                    res.set("Vary", "User-Agent");
+                    res.send(body.toString());
+                })
+                    .catch(err => {
+                    throw new Error(err);
+                });
+            }
+            else {
+                if (isIE(userAgent)) {
+                    res.send(ht_app_browser_not_supported_1.browserNotSupported(appName, ico64, ico32, svg));
+                }
+                else {
+                    res.send(pwashell);
+                }
+                // fetch(`https://01.ht/app.html`)
+                //   .then(function(res) {
+                //     return res.text();
+                //   })
+                //   .then(function(body) {
+                //     res.send(body.toString());
+                //   });
+            }
         }
-        // fetch(`https://01.ht/app.html`)
-        //   .then(function(res) {
-        //     return res.text();
-        //   })
-        //   .then(function(body) {
-        //     res.send(body.toString());
-        //   });
-      }
-    } catch (err) {
-      res.send("Error:" + err);
-      console.log("Error:" + err);
-    }
-  });
-  return app;
+        catch (err) {
+            res.send("Error:" + err);
+            console.log("Error:" + err);
+        }
+    });
+    return app;
 }
-
-export { middleware };
+exports.middleware = middleware;
+//# sourceMappingURL=middleware.js.map
--- store.ts
+++ store.ts
@@ -1,22 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const admin = require("firebase-admin");
-exports.admin = admin;
-const firebase_functions_1 = require("firebase-functions");
+import * as admin from "firebase-admin";
+import { config } from "firebase-functions";
+
 // Initialize the default app
 admin.initializeApp();
 // const defaultApp = admin.initializeApp();
 // const defaultAuth = defaultApp.auth();
 // const defaultDatabase = defaultApp.database();
+
 // Initialize the main app
-const firebaseConfig = firebase_functions_1.config().config;
-const serviceAccount = firebase_functions_1.config().service_account;
+const firebaseConfig = config().config;
+const serviceAccount = config().service_account;
 // Need replace \\n - https://github.com/firebase/firebase-tools/issues/371
 serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 firebaseConfig.credential = admin.credential.cert(serviceAccount);
 const mainApp = admin.initializeApp(firebaseConfig, "mainApp");
+
 const auth = mainApp.auth();
-exports.auth = auth;
 const db = mainApp.firestore();
-exports.db = db;
-//# sourceMappingURL=store.js.map
+
+export { admin, auth, db };
